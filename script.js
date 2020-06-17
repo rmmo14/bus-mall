1f4a4d80d7fdf9e49c4e3b39c812094871cb3646
Rene Montoya <rmontoya.we@gmail.com>
2020-06-16 18:35:44 -0700

    finising lab11

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-// ============= prompt 1 ==========
 var lineUp = [];
 var tClicks = 0;
-var maxClicks = 0;
+var maxClicks = 5;
+var randomNumArray = [];
 
-// create a constructor function that creates an object associated with each product and has: name of product and file path of image
+
 function Produce(imageSource, caption) {
   this.clicked = 0;
   this.shown = 0;
-  this.imageSource = imageSource;
+  this.imageSrc = imageSource;
   this.caption = caption;
+  this.percent = 0;
 
   lineUp.push(this);
 }
@@ -16,27 +17,23 @@ function Produce(imageSource, caption) {
 new Produce('img/bag.jpg', 'R2D2 Bag');
 new Produce('img/banana.jpg', 'Banana Slicer');
 new Produce('img/bathroom.jpg', 'Ipad Stand');
-// new Produce('img/boots.jpg', 'Rainfull Boots');
-// new Produce('img/breakfast.jpg', 'BeFast');
-// new Produce('img/bubblegum.jpg', 'Meatball Gum');
-// new Produce('img/chair.jpg', 'Hyperbola Chair');
-// new Produce('img/cthulu.jpg', 'Mighty Cthulu');
-// new Produce('img/dog-duck.jpg', 'Dog Quack');
-// new Produce('img/dragon.jpg', 'Dragon Meat');
-// new Produce('img/pen.jpg', 'Utensil Pens');
-// new Produce('img/pet-sweep.jpg', 'Paw Brooms');
-// new Produce('img/scissors.jpg', 'Pizssors');
-// new Produce('img/shark.jpg', 'Sleepin Shark');
-// new Produce('img/sweep.jpg', 'Baby Sweeper');
-// new Produce('img/tauntaun.jpg', 'Sleeping Star Wars');
-// new Produce('img/unicorn.jpg', 'Uni Meat');
-// new Produce('img/usb.gif', 'USB');
-// new Produce('img/water-can.jpg', 'Water Can');
-// new Produce('img/wine-glass.jpg', 'Wine');
-
-// when user clicks product it generates three new products to pick from
-// create an algorithm that will randomly generate 3 unique product images from the images directory and display them side-by-side
-// attach an event listener to the section where the images are displayed
+new Produce('img/boots.jpg', 'Rainfull Boots');
+new Produce('img/breakfast.jpg', 'BeFast');
+new Produce('img/bubblegum.jpg', 'Meatball Gum');
+new Produce('img/chair.jpg', 'Hyperbola Chair');
+new Produce('img/cthulhu.jpg', 'Mighty Cthulu');
+new Produce('img/dog-duck.jpg', 'Dog Quack');
+new Produce('img/dragon.jpg', 'Dragon Meat');
+new Produce('img/pen.jpg', 'Utensil Pens');
+new Produce('img/pet-sweep.jpg', 'Paw Brooms');
+new Produce('img/scissors.jpg', 'Pizssors');
+new Produce('img/shark.jpg', 'Sleepin Shark');
+new Produce('img/sweep.png', 'Baby Sweeper');
+new Produce('img/tauntaun.jpg', 'Sleeping Star Wars');
+new Produce('img/unicorn.jpg', 'Uni Meat');
+new Produce('img/usb.gif', 'USB');
+new Produce('img/water-can.jpg', 'Water Can');
+new Produce('img/wine-glass.jpg', 'Wine');
 
 function randomizer(min, max) {
   min = Math.ceil(min);
@@ -44,93 +41,81 @@ function randomizer(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-
-var figureElement = document.getElementById('product-imgs');
-figureElement.addEventListener('click', handleProdClick);
+var figureSection = document.getElementById('product-imgs');
+figureSection.addEventListener('click', handleProdClick);
 
 function handleProdClick(event) {
-  if (event.target.tagName === 'IMG') {
-    tClicks++;
-    console.log(tClicks);
-
-    var imgElement = event.targetAttribute('src');
-    for (var i = 0; i < lineUp.length; i++) {
-      if (lineUp[i].imageSource === imgElement) {
-        // this  increases the objects this.clicked attribute
-        lineUp[i].clicked++;
-      }
-    }
-    // this will remove event listener when reach max clicks allowed
-    if (tClicks === maxClicks) {
-      figureElement.removeEventListener('click', handleProdClick);
-    }
-    renderProdImg();
+  // if(userClick.target.tagName === 'IMG'){
+  //   tClicks++;
+  // }
+  if (tClicks === maxClicks) {
+    figureSection.removeEventListener('click', handleProdClick);
+    showResults();
   }
+
+  var targetSource = event.target.getAttribute('src');
+  for (var i = 0; i < lineUp.length; i++) {
+    if (lineUp[i].imageSrc === targetSource) {
+      lineUp[i].clicked++;
+    }
+  }
+  tClicks++;
+  reRenderRandImg();
 }
 
-function renderProdImg() {
+
+function reRenderRandImg() {
   var firstProdImg = randomizer(0, lineUp.length);
   var secondProdImg = randomizer(0, lineUp.length);
   var thirdProdImg = randomizer(0, lineUp.length);
 
-  // use a loop to ensure 1st neq 2nd neq 3rd number to use for position of images
   while (firstProdImg === secondProdImg || secondProdImg === thirdProdImg || firstProdImg === thirdProdImg) {
     firstProdImg = randomizer(0, lineUp.length);
     secondProdImg = randomizer(0, lineUp.length);
+    thirdProdImg = randomizer(0, lineUp.length);
+  }
+  randomNumArray[0] = firstProdImg;
+  randomNumArray[1] = secondProdImg;
+  randomNumArray[2] = thirdProdImg;
+  console.log(randomNumArray, leftOption, event);
+  // debugger;
+  var leftImg = document.getElementById('leftimg');
+  var leftTxt = document.getElementById('lefttext');
+  var leftOption = lineUp[firstProdImg];
+  leftImg.src = leftOption.imageSrc;
+  leftTxt.textContent = leftOption.caption;
+  leftOption.shown++;
+
+  var centImg = document.getElementById('centerimg');
+  var centTxt = document.getElementById('centertext');
+  centImg.src = lineUp[secondProdImg].imageSrc;
+  centTxt.textContent = lineUp[secondProdImg].caption;
+  lineUp[secondProdImg].shown++;
+
+  var rightImg = document.getElementById('rightimg');
+  var rightTxt = document.getElementById('righttext');
+  rightImg.src = lineUp[thirdProdImg].imageSrc;
+  rightTxt.textContent = lineUp[thirdProdImg].caption;
+  lineUp[thirdProdImg].shown++;
+
+}
+
+
+// The results
+function showResults() {
+  for (var i = 0; i < lineUp.length; i++) {
+    var resultsData = document.getElementById('clicks');
+    var resultsContent = document.createElement('li');
+    resultsContent.textContent = lineUp[i].caption + ' - shown: ' + lineUp[i].shown + ' , clicked: ' + lineUp[i].clicked;
+    resultsData.appendChild(resultsContent);
   }
 }
 
-var leftImg = document.getElementById('leftimg');
-var leftTxt = document.getElementById('lefttext');
-var centerImg = document.getElementById('centerimg');
-var centerTxt = document.getElementById('centertext');
-var rightImg = document.getElementById('rightimg');
-var rightTxt = document.getElementById('righttext');
-
-var firstProduct = lineUp[firstProdImg];
-leftImg.src = firstProduct.imageSource;
-leftTxt.textContent = firstProduct.caption;
-firstProduct.shown++;
-
-var secondProduct = lineUp[secondProdImg];
-centerImg.src = secondProduct.imageSource;
-centerTxt.textContent = secondProduct.caption;
-secondProduct.shown++;
-
-var thirdProduct = lineUp[thirdProdImg];
-rightImg.src = thirdProduct.imageSource;
-rightTxt.textContent = thirdProduct.caption;
-thirdProduct.shown++;
-
-// The results 
-
-var resultsData = document.getElementById('clicks');
-var resultsHeader = document.createElement('h3');
-var resultsContent = document.createElement('li');
-
-resultsContent.textContent = 'Survey Says';
-resultsData.appendChild(resultsHeader);
-
-for (var i = 0; i < lineUp.length;)
-
-// ================ prompt 2 =============
-
-//track the selections made by viewers to determine which products to keep
-// in the constructor function define a property to hold the number of times a product has been clicked
-// after every selection by the viewer, update the newly added property to reflect if it was clicked
 
 
 // ========== prompt 3 ==============
 
 //control the number of rounds a user is presented with so that can control the voting session duration
-// by default: provide 25 rounds of voting before ending the session
 // keep the number of rounds variable to allow the number to be easily changed for debugging and testing
 
 
-
-// ============= prompt 4 ==============
-
-// user should be able to view a report of results after all rounds of voting have concluded
-// create a property attached to the constructor function that keeps track of all the products that are being considered
-// after voiting rounds, remove the event listeners on the product
-// display the list of all the products followed by the votes received and number of times seen for each
